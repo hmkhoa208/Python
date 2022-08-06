ac32b4c769f2b88ef2eed24d5a9dd95cb5a0a6dc
hmkhoa208 <hmkhoa208@gmail.com>
2022-08-06 15:12:17 +0700

    update

--- selenium_bs4.py
+++ selenium_bs4.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-# import json
+
+
 
 # Lay BeautifulSoup cua website
 def getSoup(site):
 	driver = webdriver.Firefox()
 	# driver.maximize_window()
 	driver.get(site)
-	time.sleep(5)
+	time.sleep(2)
 	soup = BeautifulSoup(driver.page_source,'lxml')
 	driver.quit()
 	return soup
@@ -17,6 +18,7 @@ def getSoup(site):
 #----------------------------------------
 
 if __name__ == '__main__':
-    with open("html_result.html", "w") as file:
-        file.write(str(getSoup('https://www.youtube.com/')))
-    # print(getSoup('https://www.google.com/'))
+    # with open("html_result.html", "w") as file:
+    #     file.write(str(getSoup('https://www.youtube.com/')))
+    soup = getSoup('https://www.google.com/')
+    print(soup.find('title').text)
